80a9586776efc35eba11cbbf47d6c77ab1bf4040
Francisco Castillo <fjcasti1@asu.edu>
2020-10-22 22:08:00 -0700

    Backend Routes

---
+++ routes/api/contacts.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const router = express.Router();
+
+// @route     GET /api/contacts
+// @desc      Get all users contacts
+// @access    Private
+router.get('/', (req, res) => {
+  res.send('Get all contacts');
+});
+
+// @route     POST /api/contacts
+// @desc      Add new contact
+// @access    Private
+router.post('/', (req, res) => {
+  res.send('Add a contact');
+});
+
+// @route     PUT /api/contacts/:d
+// @desc      Update a contact
+// @access    Private
+router.put('/:id', (req, res) => {
+  res.send('Update contact');
+});
+
+// @route     DELETE /api/contacts/:id
+// @desc      Delete a contact
+// @access    Private
+router.delete('/:id', (req, res) => {
+  res.send('Register a user');
+});
+
+module.exports = router;
