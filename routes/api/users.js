dd39ae43cb8b5cc122a6abf58bda642435ddd1a9
Francisco Castillo <fjcasti1@asu.edu>
2020-10-22 23:08:56 -0700

    Hash password & Register Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../../models/User');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
@@ -16,12 +17,33 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('Register a user');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      // Create new user instance
+      user = new User({ name, email, password });
+
+      // Hash password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save in DB
+      await user.save();
+
+      res.json({ user });
+    } catch (err) {}
   },
 );
 
