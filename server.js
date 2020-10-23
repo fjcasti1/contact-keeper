9a59e3f674eefff795c564f5fa8ce11eddec5eac
Francisco Castillo <fjcasti1@asu.edu>
2020-10-22 22:17:47 -0700

    Connect mongoDB to the App

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to the Contact Keeper API...' });
-});
+// Connect Database
+connectDB();
 
 // Define Routes
 app.use('/api/auth', require('./routes/api/auth'));
@@ -13,4 +13,4 @@ app.use('/api/contacts', require('./routes/api/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`-->  Server running on port ${PORT}`));
